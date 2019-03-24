10653f2f586b39a3322d4b1225477a2955a44c8e
Arkadiusz1988 <arkadiusz.hojenski@gmail.com>
2019-03-24 18:08:11 +0100

    first commit

---
+++ First.py
@@ -0,0 +1,33 @@
+# print("This line will be printed.")
+#
+# x = 1
+# if x == 1:
+#     # indented four spaces
+#     print("x is 1.")
+#
+#
+# def generator_function():
+#     for i in range(10):
+#         yield i
+#
+# for item in generator_function():
+#     print(item)
+#
+# wzrost = 1.60
+# waga = 53
+# BMI = waga / (wzrost ** 2)
+# print("Twoje bmi wynosi:",BMI)
+#
+# print("podaj")
+# cos=input()
+# print(cos)
+
+tmp=12
+tmp1='15'
+tmp2=' gosia ma kota'
+
+print(2*tmp2)
+
+print(tmp+int(tmp1))
+
+print(2*tmp+'gosiaczek')
---
+++ lesson_01_zad_dom.py
@@ -0,0 +1,12 @@
+# Pobieranie wartości zmiennej z klawiatury
+name = input('Podaj swoje imię: ')
+print('Nazywasz się', name + '.')
+
+# Pobieranie wartości zmiennej z klawiatury
+yearOfBirth = input('Podaj swoj rok urodzenia: ')
+print('twoj rok urodzenia to:', yearOfBirth + '.')
+
+print('twoj wiek to:', 2019 - int(yearOfBirth))
+
+# Zakończenie
+input('Aby zakończyć naciśnij klawisz ENTER.')
